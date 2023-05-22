3a6f50416d0b84c6e38a0cb8336dacb4b914a68d
vikas <vikaspatil212222@gmail.com>
2023-05-22 10:07:14 +0530

    main

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ import cors from "cors";
 const corsOptions = {
     credentials: true,            //access-control-allow-credentials:true
     optionSuccessStatus: 200,
-    origin: ["http://localhost:3000"]
+    origin: ["http://localhost:3000","https://internship-round2.vercel.app"]
 
 }
 
